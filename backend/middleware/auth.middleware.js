0f88201c9f1de4a7fa4381e215bb2e69a218a7e6
Akarshana06 <akarshana027@gmail.com>
2025-05-15 12:30:26 +0530

    jwt token done

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -4,12 +4,12 @@ const User = require('../models/User');
 exports.protect = async (req, res, next) => {
   let token;
 
-  // Check if the Authorization header exists and starts with "Bearer"
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
-    token = req.headers.authorization.split(' ')[1]; // Extract the token
+    token = req.headers.authorization.split(' ')[1];
   }
 
   if (!token) {
@@ -17,17 +17,18 @@ exports.protect = async (req, res, next) => {
   }
 
   try {
-    // Verify the token
+    
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Attach the user to the request object
+    
     req.user = await User.findById(decoded.id).select('-password');
 
     if (!req.user) {
       return res.status(401).json({ message: 'Not authorized, user not found' });
     }
 
-    next(); // Proceed to the next middleware or route handler
+    next();
   } catch (err) {
     console.error(err);
     return res.status(401).json({ message: 'Not authorized, token failed' });
